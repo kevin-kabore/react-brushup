c1aafabd61f0c5d0da046cd96ea2d447543c7ce0
Kevin Kabore <kevin@tvgla.com>
2019-06-14 16:27:49 -0700

    Adds reducer with useReducer hook

--- hooks-todos/src/App.js
+++ hooks-todos/src/App.js
@@ -1,12 +1,38 @@
-import React, { useContext} from 'react';
-import {UserContext} from './index'
+import React, { useContext, useReducer } from 'react';
+import { UserContext } from './index'
 
+const initialState = {
+  count: 0
+}
+
+function countReducer(state, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      }
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      }
+    default:
+      return initialState
+
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(countReducer, initialState)
   const value = useContext(UserContext)
+
   return (
     <div>
-      Hi {value}
+      <div>
+        Hi {value}
+      </div>
+      <div>{state.count}</div>
+      <button onClick={() => dispatch({ type: 'INCREMENT' })}>Increment</button>
+      <button onClick={()=> dispatch({ type:'DECREMENT' })}>Decrement</button>
     </div>
   );
 }
