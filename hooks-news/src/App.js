45e2255b0498aecb0cca9ff32be87ca08bfe6eb2
Kevin Kabore <kevin@tvgla.com>
2019-06-14 15:47:49 -0700

    Adds styling with tailwindcss

--- hooks-news/src/App.js
+++ hooks-news/src/App.js
@@ -50,34 +50,40 @@ export default function App() {
   // }, [])
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
+    <div className="container max-w-3xl mx-auto p-4 m-2 bg-purple-200 shadow-lg rounded">
+      <img
+        src="https://icon.now.sh/react/c0c"
+        className="float-right h-12"
+        />
+      <h1 className="text-grey-600 font-thin text-5xl">hooks news</h1>
+      <form onSubmit={handleSubmit} className="mb-2">
         <input
           type="text"
           onChange={(e) => setQuery(e.target.value)}
           value={query}
           ref={searchInputRef}
+          className="border p-1 rounded"
         />
-        <button type="submit">
+      <button type="submit" className="bg-orange-300 rounded m-1 p-1">
           Search
         </button>
-        <button type="button" onClick={handleClearSearch}>
+        <button type="button" className="bg-teal-300 text-white rounded m-1 p-1" onClick={handleClearSearch}>
           Clear
         </button>
       </form>
       {loading ? (
-        <div>Loading results...</div>
+        <div className="font-bold text-orange-600">Loading results...</div>
       ): (
-        <ul>
+        <ul className="list-reset leading-normal">
           {results.map(result => (
             <li key={result.objectID}>
-              <a href={result.url}>{result.title}</a>
+              <a href={result.url} className="text-indigo-700 hover:text-indigo-900">{result.title}</a>
             </li>
           ))}
         </ul>)
       }
 
-      {error && <div>{error.message}</div>}
-    </>
+      {error && <div className="text-red-500 font-bold">{error.message}</div>}
+    </div>
   );
 }
