309423b25bd5cd93867f6f0d5b2586b8bd86fff5
Kevin Kabore <kevin.s.kabore@gmail.com>
2019-06-15 00:42:51 -0700

    Adds toggleTodo action

--- hooks-todos/src/components/TodoList.js
+++ hooks-todos/src/components/TodoList.js
@@ -1,16 +1,44 @@
 import React, { useContext } from 'react';
-import TodosContext from '../context'
+import TodosContext from '../context';
 
-export default function TodoList () {
-  const {state} = useContext(TodosContext)
+export default function TodoList() {
+  const { state, dispatch } = useContext(TodosContext);
 
+  const title =
+    state.todos.length > 0 ? `${state.todos.length} Todos` : 'Nothing to do.';
   return (
-    <div>
-      {state.todos.map(todo => (
-        <li key={todo.id}>
-          <span>{todo.text}</span>
-        </li>
-      ))}
+    <div className="container mx-auto max-w-md text-center font-mono">
+      <h1 className="text-bold text-4xl">{title}</h1>
+      <ul className="text-white">
+        {state.todos.map(todo => (
+          <li
+            key={todo.id}
+            className="flex items-center bg-orange-500 border-black border-dashed border-2 my-2 py-3">
+            <span
+              onDoubleClick={() =>
+                dispatch({ type: 'TOGGLE_TODO', payload: todo })
+              }
+              className={`cursor-pointer flex-1 ml-12 ${todo.complete &&
+                'line-through text-black'}`}>
+              {todo.text}
+            </span>
+            <button>
+              <img
+                src="https://icon.now.sh/edit/0050c5"
+                alt="Edit Icon"
+                className="h-6 "
+              />
+            </button>
+            <button>
+              <img
+                src="https://icon.now.sh/delete/8b0000"
+                alt="Delete Icon"
+                className="h-6 "
+              />
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- hooks-todos/src/reducer.js
+++ hooks-todos/src/reducer.js
@@ -1,7 +1,19 @@
 export default function todosReducer(state, action) {
   switch (action.type) {
-    case 'ADD_TODO':
-
+    case 'TOGGLE_TODO':
+      const toggledTodos = state.todos.map(
+        todo =>
+          todo.id === action.payload.id
+            ? {
+                ...action.payload,
+                complete: !action.payload.complete
+              }
+            : todo
+      );
+      return {
+        ...state,
+        todos: toggledTodos
+      };
       break;
     default:
       return state;
